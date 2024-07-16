5704294d11585a20af810d8360237ad22ea3d143
Tomacho <tomacho28.th@gmail.com>
2024-07-15 21:02:09 -0300

    terminamos ejercisio1

---
+++ js/ejercisio1.js
@@ -0,0 +1,9 @@
+const edad = parseInt(prompt("Ingrese su edad"));
+
+if (edad >= 18 ) 
+    {
+     document.write("Puede conducir");
+    }
+else {
+    document.write("La edad ingresada no es valida para conducir");
+}    
