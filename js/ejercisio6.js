2754d62341c0d3f7898205d45e860dc4f6863d00
Tomacho <tomacho28.th@gmail.com>
2024-07-16 19:43:09 -0300

    ejercisio 6

---
+++ js/ejercisio6.js
@@ -0,0 +1,15 @@
+var i, repeticion;
+
+for(i =0; i<=30; i++) 
+    {
+        for(repeticion = 0; repeticion < i; repeticion ++)
+            {
+            document.write(i); 
+        }
+
+
+        document.write("<br>")
+
+    }
+
+  
