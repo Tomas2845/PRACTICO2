75d6dd1e00241a8319417e9fcabff6cfc8d30dd7
Tomacho <tomacho28.th@gmail.com>
2024-07-16 01:01:26 -0300

    terminamos ejercisio 2

---
+++ js/ejercisio2.js
@@ -0,0 +1,42 @@
+const numero = parseInt(prompt("Ingrese un numero del 1 al 10"));
+
+switch (numero) {
+  case 0:
+    alert("muy deficiente")
+    break;
+  case 1:
+    alert("muy deficiente")
+    break;
+  case 2:
+    alert("muy deficiente")
+    break;
+  case 3:
+    alert("insuficiente")
+    break;
+  case 4:
+    alert("insuficiente")
+    break;
+  case 5:
+    alert("suficiente")
+    break;
+  case 6:
+    alert("suficiente")
+    break;
+  case 7:
+    alert("bien")
+    break;
+  case 8:
+    alert("notable")
+    break;
+  case 9:
+    alert("notable")
+    break;
+  case 10:
+    alert("sobresaliente")
+    break;
+    default:
+        alert("numero erroneo")
+        alert("ingrese un numero valido")
+
+
+}
